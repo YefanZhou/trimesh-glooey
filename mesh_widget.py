efdbfe5fed51e7b7d54d4cc274f9648e4ffce832
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-20 17:22:55 +0100

    Drag to rotate mesh

--- mesh_widget.py
+++ mesh_widget.py
@@ -1,4 +1,5 @@
 import glooey
+import numpy as np
 import pyglet
 import trimesh
 
@@ -108,11 +109,14 @@ class SceneGroup(pyglet.graphics.Group):
 
 class MeshGroup(pyglet.graphics.Group):
 
-    def __init__(self, transform=None, parent=None):
+    def __init__(self, transform=None, view_transform=None, parent=None):
         super().__init__(parent)
         if transform is None:
             transform = np.eye(4)
         self.transform = transform
+        if view_transform is None:
+            view_transform = np.eye(4)
+        self.view_transform = view_transform
 
     def set_state(self):
         from trimesh.rendering import matrix_to_gl
@@ -120,6 +124,7 @@ class MeshGroup(pyglet.graphics.Group):
         glPushMatrix()
 
         glLoadIdentity()
+        glMultMatrixf(matrix_to_gl(self.view_transform))
         glMultMatrixf(matrix_to_gl(self.transform))
 
     def unset_state(self):
@@ -138,14 +143,22 @@ class MeshWidget(glooey.Widget):
         self.vertex_list = None
         self.mesh_group = None
 
+        from trimesh.transformations import Arcball
+
+        self.view = {'translation': np.zeros(3),
+                     'center': self.mesh.centroid,
+                     'scale': self.mesh.scale,
+                     'ball': Arcball()}
+
     def do_claim(self):
         return 0, 0
 
     def do_regroup(self):
         if self.vertex_list is not None:
             self.mesh_group = MeshGroup(
-                SceneGroup(rect=self.rect, parent=self.group),
                 transform=transform,
+                view_transform=view_to_transform(self.view),
+                parent=SceneGroup(rect=self.rect, parent=self.group),
             )
             self.batch.migrate(
                 self.vertex_list,
@@ -162,6 +175,7 @@ class MeshWidget(glooey.Widget):
         if self.vertex_list is None:
             self.mesh_group = MeshGroup(
                 transform=self.transform,
+                view_transform=view_to_transform(self.view),
                 parent=SceneGroup(rect=self.rect, parent=self.group),
             )
             args = mesh_to_vertexlist(self.mesh, group=self.mesh_group)
@@ -172,8 +186,44 @@ class MeshWidget(glooey.Widget):
             self.vertex_list.delete()
             self.vertex_list = None
 
+    def on_mouse_press(self, x, y, buttons, modifiers):
+        self.view['ball'].down([x, -y])
+        if self.mesh_group:
+            self.mesh_group.view_transform = view_to_transform(self.view)
+        self._draw()
 
-if __name__ == '__main__':
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        # detect crossing edge between widgets
+        x_prev = x - dx
+        y_prev = y - dy
+        left, bottom = self.rect.left, self.rect.bottom
+        width, height = self.rect.width, self.rect.height
+        if not (left <= x_prev <= left + width) or \
+                not (bottom <= y_prev <= bottom + height):
+            self.view['ball'].down([x, y])
+
+        # left mouse button, with control key down (pan)
+        if ((buttons == pyglet.window.mouse.LEFT) and
+                (modifiers & pyglet.window.key.MOD_CTRL)):
+            delta = [dx / self.rect.width, dy / self.rect.height]
+            self.view['translation'][:2] += delta
+        # left mouse button, no modifier keys pressed (rotate)
+        elif (buttons == pyglet.window.mouse.LEFT):
+            self.view['ball'].drag([x, -y])
+        if self.mesh_group:
+            self.mesh_group.view_transform = view_to_transform(self.view)
+        self._draw()
+
+
+def view_to_transform(view):
+    transform = view['ball'].matrix()
+    transform[:3, 3] = view['center']
+    transform[:3, 3] -= np.dot(transform[:3, :3], view['center'])
+    transform[:3, 3] += view['translation'] * view['scale']
+    return transform
+
+
+def main():
     import trimesh.transformations as tf
 
     window = pyglet.window.Window(width=1280, height=480)
@@ -181,16 +231,23 @@ if __name__ == '__main__':
 
     hbox = glooey.HBox()
 
-    transform = tf.translation_matrix([0, 0, -10])
+    t = tf.translation_matrix([0, 0, -1])
 
-    # hbox.add(glooey.Placeholder(min_width=640, min_height=480))
-    mesh = trimesh.creation.annulus(0.2, 1, 0.2)
-    hbox.add(MeshWidget(mesh, transform=transform))
+    mesh = trimesh.creation.axis(0.03)
+    hbox.add(MeshWidget(mesh, transform=t @ tf.random_rotation_matrix()))
 
-    mesh = trimesh.creation.axis(0.3)
-    hbox.add(MeshWidget(mesh, transform=transform))
-    # hbox.add(glooey.Placeholder(min_width=640, min_height=480))
+    hbox.add(
+        glooey.Placeholder(min_width=5, min_height=480, color=(0, 0, 0)),
+        size=0
+    )
+
+    mesh = trimesh.creation.axis(0.03)
+    hbox.add(MeshWidget(mesh, transform=t @ tf.random_rotation_matrix()))
 
     gui.add(hbox)
 
     pyglet.app.run()
+
+
+if __name__ == '__main__':
+    main()
